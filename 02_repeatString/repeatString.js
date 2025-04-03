b4086fc1218745b2ace4bfa22d63025dfafdac6b
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-03 11:09:49 +0530

    Populate the given empty function with logic
    Fulfils the test requirements mentioned in the
    spec file.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num<0){
+        return "ERROR"
+    }else{
+        let outputString="";
+        for (let i=0;i<num;i++){
+            outputString+=string;
+        }
+        return outputString;
+
+    }
+
 
 };
-
 // Do not edit below this line
 module.exports = repeatString;
