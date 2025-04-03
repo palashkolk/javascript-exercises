bb69136f05c520271388b7f28c48473c5674e0f7
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-03 16:10:38 +0530

    Populates convertToCelcius and convertToFahrenheit
    Fulfils the javascript exercise 7 requirement of
    the ODIN project.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let temperatureInCelcius=(temperatureInFahrenheit -32)*100/180;
+  return Number(temperatureInCelcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelcius) {
+  let temperatureInFahrenheit=(temperatureInCelcius-0)*180/100+32;
+  return Number(temperatureInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
