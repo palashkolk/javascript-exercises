b1cb0f8f1017554e9b299f31a0752643d7ce46a4
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-03 16:35:24 +0530

    Populates different functions with code
    Fulfills javascript exercise 8 requirements of the
    ODIN project. Satisfies test requirements.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,52 @@
-const add = function() {
+const add = function(number1, number2) {
+  return number1+ number2;
 	
 };
 
-const subtract = function() {
+const subtract = function(number1, number2) {
+  return number1-number2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let sum=0;
+  for (let element of array){
+    sum+=element;
+  }
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let sum=1;
+  for (let element of array){
+    sum*=element;
+  }
+  return sum;
 
 };
 
-const power = function() {
+const power = function(number1, number2) {
+  let sum=1;
+  for (let i=0; i< number2; i++){
+    sum*=number1;
+
+  } 
+  return sum;
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  if(number==0){
+    return 1;
+  }else{
+    let sum=1;
+    while(number>0){
+      sum *=number;
+      number--;
+    }
+    return sum;
+  }
 	
 };
 
