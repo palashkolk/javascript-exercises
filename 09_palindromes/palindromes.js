0cc7dbe94cb614f3a68561e6acb9e52dedb9f516
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-03 17:43:36 +0530

    Populates palindrome funct, uses removeFromArray
    Fulfils javascript exercise 9 requirement of the
    ODIN project. Makes use of earlier created function
    removeFromArray.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const stringArray=removeFromArray(string.toLowerCase().split(""), "!", ",", ".", " ", "-");
+    const cleanedString=stringArray.join("");
+    const reverseString=stringArray.reverse().join("");
+    console.log(reverseString, cleanedString)
+    if (reverseString==cleanedString){
+        return true;
+    }else{
+        return false;
+    }
+    
 };
+const removeFromArray = function(array, ...args) {
+    for (let arg of args){
+        let index = array.indexOf(arg);
+        while(index > -1){
+            array.splice(index, 1);
+            index = array.indexOf(arg);
+        }
 
+    }
+    return array;
+};
+palindromes("A car, a man, a maraca.")
 // Do not edit below this line
 module.exports = palindromes;
