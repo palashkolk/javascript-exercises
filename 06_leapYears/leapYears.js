07dc3e9978fb94030cbba995ac6bd1cf678c96be
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-03 15:45:24 +0530

    Populates leapYears function with logic
    Fulfils javascript exercise 6 requirement of the
    ODIN project. Satisfies all the tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (typeof(year)=="number"){
+        if(year%4==0){
+            if(year%100==0){
+                if (year%400==0){
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return "ERROR";
+    }
 
 };
 
