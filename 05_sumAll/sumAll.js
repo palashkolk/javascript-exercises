c9ae6ffb2967eb4e6ed55bbd26716a4d56fb5b76
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-03 14:03:07 +0530

    Populates the sumAll function with logic
    The code fulfils the requirement of the the ODIN
    project javascript exercise 5 and satisfies all
    the tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if(number1<0 || number2<0 ||(Math.floor(number1)!=number1)||(Math.floor(number2)!=number2)|| typeof(number1)!= "number"||typeof(number2)!= "number"){
+        return "ERROR";
+    }else{
+        let startingNumber;
+        let endingNumber;
+        if(number1>number2){
+            startingNumber=number2;
+            endingNumber=number1;
+            
+        }else{
+            startingNumber=number1;
+            endingNumber=number2;
+            
+        }
+        let sum=0;
+        while(startingNumber <= endingNumber) {
+            sum+=startingNumber;
+            startingNumber++;
+        }
+        return sum;
+    }
+
 
 };
-
+sumAll(4,1);
 // Do not edit below this line
 module.exports = sumAll;
