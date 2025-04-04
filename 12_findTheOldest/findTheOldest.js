3c92dc6a0d44a389d5ea8b309d971756ef322c38
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-04 13:57:25 +0530

    Populates the findTheOldest function
    Fulfils js exercise 12 requirement of the ODIN
    project. Satisfies all the tests.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let age;
+    let cleanedArray=[];
+    let ageArray=[];
+    for (let object of array ){
+        if (typeof(object.yearOfDeath)=="undefined"){
+            const yearOfDeath = new Date().getFullYear();
+            age=yearOfDeath-object.yearOfBirth;
 
+        }else{
+            age=object.yearOfDeath-object.yearOfBirth;
+        }
+        cleanedArray.push({"name":object.name, "age":age});
+        ageArray.push(age);
+    }
+    const maxAge=ageArray.reduce(getMax);
+    
+    for (let element of cleanedArray){
+        if(element.age==maxAge){
+            return element;
+        }
+    }
+    
 };
+
+function getMax(num1, num2){
+    if(num1>num2){
+        return num1;
+    }else{
+        return num2;
+    }
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
