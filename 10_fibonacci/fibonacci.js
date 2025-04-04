68ab3d4b20e0e5c7835101f1ebc58bdf46074f1f
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-04 11:22:41 +0530

    Populates the given fibonacci function
    Fulfilts the javascript exercise 10 requirement of
    the ODIN project. Satisfies all the tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    const termInSeries= Number(term);
+    const zerothTerm=0;
+    const firstTerm=1;
+    let sum1 =firstTerm;
+    let sum2 =zerothTerm;
+    if (termInSeries<0){
+        return "OOPS"
+    }
+    else if(termInSeries==0){
+        return zerothTerm;
+    }else if (termInSeries==1){
+        return firstTerm;
+    }else{
+        for (let i=2;i<=termInSeries;i++){
+            sum = sum1+sum2; 
+            sum2=sum1;
+            sum1=sum;                                                                                                                                                                 
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
