f02479b37b496f0015ab5ef8f798e406324d32ce
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-04 11:46:02 +0530

    Populate the function getTheTitles
    Fulfils javascript exercise 11 requirements of
    the ODIN project. Satisfie the tests.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let titles=[];
+    for (let member of array){
+        titles.push(member.title);
+    }
+    return titles;
 
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
